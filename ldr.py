73b4a845adbb4399fa2f79bd6217ec8b7c85fad0
Alec <frstrtr@gmail.com>
2020-02-07 03:20:22 +0300

    Update ldr.py

--- ldr.py
+++ ldr.py
@@ -8,11 +8,14 @@ from os import system, name
 
 # import sleep to show output for some time period
 from time import sleep
-import keys
+
+# import account related keys for sign operations
+from keys import host, organisation_id, secret, key
 
 BTCpairFlag = False  # Pair Flag for BTC is sec = False, pri = True
 MIN_BTC_TRADE = 0.0001  # minimal BTC trade size
 current_market = 12  # ETHBTC by default
+manual_order_size = None # Automatical calculation
 
 # Create public api object
 public_api = nicehash.public_api(host, False)
@@ -54,7 +57,7 @@ def get_market_list():
 def list_non_zero_balances():
     ''' Get balance for all currencies with funds'''
     currencies = public_api.get_curencies()
-    my_accounts = private_api.get_accounts()
+    # my_accounts = private_api.get_accounts()
     # print('\nMy accounts:')
     # print(my_accounts)
     # print ('\n')
@@ -94,7 +97,7 @@ def market_select():
     return selected_market
 
 
-def list_all_my_open_orders():
+def list_my_all_open_orders():
     '''Print all open order on Exchange
     Checks for delisted BSV'''
     # Get my open exchange orders
@@ -116,6 +119,78 @@ def list_all_my_open_orders():
 
             for n in my_exchange_orders:  # n is a dictionary of trade order data
                 print(n)
+
+
+def get_latest_shift_advice(direction='buy'):
+    """Refresh latest trade shift advice limit price and minimal amount
+
+    Returns:
+        [type] -- [description]
+        edge_price - price advice
+        edge_amount - amount, calculated to minimal trade size of 0.0001 BTC
+
+    """
+    # Refresh exchange info - do we really need it?
+    exchange_info = public_api.get_exchange_markets_info()
+
+    # Refresh exchange orderbook
+    exchange_orderbook = public_api.get_exchange_orderbook(
+        exchange_info['symbols'][int(current_market)]['symbol'], 5)  # number of last trades
+
+    # last trade price, etc.
+    # last_trade = public_api.get_exchange_last_trade(exchange_info['symbols'][int(current_market)]['symbol'])
+    # last_price = last_trade[0]['price']  # Current (last) trade price
+
+    if direction == 'buy':
+
+        # calculate edge prices
+        buy_edge_price = exchange_orderbook['buy'][0][0]
+        # buy_edge_amount = exchange_orderbook['buy'][0][1]
+
+        # Display BUY advice
+
+        limit_price = round((buy_edge_price+0.00000001), 8)
+        print(bc.OKGREEN+'B U Y advice:')
+
+        # Calculate minimum trade size
+        min_trade_size = round(MIN_BTC_TRADE/limit_price, 8)
+        print('\nLimit Price:\n'+bc.BOLD)
+        print(limit_price, bc.ENDC)
+
+        print(bc.OKGREEN+'\nAmount to BUY:\n'+bc.BOLD)
+        print(min_trade_size)
+        print(bc.ENDC)
+
+    else:
+        sell_edge_price = exchange_orderbook['sell'][0][0]
+        # sell_edge_amount = exchange_orderbook['sell'][0][1]
+
+        # Display SELL advice
+
+        limit_price = round((sell_edge_price-0.00000001), 8)
+        print(bc.WARNING+'S E L L advice:')
+
+        # Calculate minimum trade size
+        min_trade_size = round(MIN_BTC_TRADE/limit_price, 8)
+
+        print('\nLimit Price:\n'+bc.BOLD)
+        print(limit_price, bc.ENDC)
+
+        print(bc.WARNING+'\nAmount to SELL:\n'+bc.BOLD)
+        print(min_trade_size)
+        print(bc.ENDC)
+
+    edge_price = limit_price
+    edge_amount = min_trade_size
+
+    return edge_price, edge_amount
+
+def percentage(part, whole):
+    return 100 * float(part)/float(whole)
+
+
+# # #                              M A I N   P R O G R A M                            # # #
+'''____________________________________________________________________________________'''
 
 
 # clear screen
@@ -229,7 +304,7 @@ while True:
 
     #print ('\nGet exchange orderbook:')
     exchange_orderbook = public_api.get_exchange_orderbook(
-        exchange_info['symbols'][int(current_market)]['symbol'], 5) # number of last trades
+        exchange_info['symbols'][int(current_market)]['symbol'], 5)  # number of last trades
     last_trade = public_api.get_exchange_last_trade(
         exchange_info['symbols'][int(current_market)]['symbol'])
     last_price = last_trade[0]['price']  # Current (last) trade price
@@ -237,17 +312,49 @@ while True:
     # print(exchange_orderbook)
     # print(current_market)
     print(bc.BOLD+'\nCurrent Market: ',
-          exchange_info['symbols'][int(current_market)]['symbol'],'\n'+bc.ENDC)
+          exchange_info['symbols'][int(current_market)]['symbol'], '\n'+bc.ENDC)
     #print ('\n')
+
+    refresh_my_exchange_orders = private_api.get_my_exchange_orders(
+        exchange_info['symbols'][int(current_market)]['symbol'], 'open')
+
+    my_order_data = list()
+    for my_order in refresh_my_exchange_orders:
+        # collect all Market Size, Price pairs in one list
+        my_order_data.append([my_order['origQty'], my_order['price']])
+
     print('Market size\tPrice (BTC)\n')
 
     for i in reversed(exchange_orderbook['sell']):
-        print ("%08.8f"%i[1],'\t',bc.WARNING, "%08.8f"%i[0], bc.ENDC)
-    print(bc.DARKGREY+'Last trade price ', "%08.8f"%last_price, bc.ENDC)
-    for i in exchange_orderbook['buy']:
-        print ("%08.8f"%i[1],'\t',bc.OKGREEN,"%08.8f"%i[0],bc.ENDC)
 
-    print ('\n\n')
+        if str(i[0]) in [pair[1] for pair in my_order_data]:
+
+            for pair in my_order_data:
+                if pair[1] == str(i[0]):
+                    my_order_size = pair[0]
+
+            print("%08.8f" % i[1], '\t', bc.WARNING,
+                  "%08.8f" % i[0], my_order_size, bc.ENDC)
+        else:
+            print("%08.8f" % i[1], '\t', bc.WARNING, "%08.8f" % i[0], bc.ENDC)
+
+    print(bc.DARKGREY+'Last trade price ', "%08.8f" % last_price, bc.ENDC)
+
+    for i in exchange_orderbook['buy']:
+
+        if str(i[0]) in [pair[1] for pair in my_order_data]: # checking if my order price in the edge of the ordrebook
+
+            for pair in my_order_data: # searching for order size corresponded to my order price
+                if pair[1] == str(i[0]):
+                    my_order_size = pair[0]
+            order_to_market_ratio = percentage(my_order_size,i[1])
+
+            print("%08.8f" % i[1], '\t', bc.OKGREEN,
+                  "%08.8f" % i[0], my_order_size, "%05.2f" % order_to_market_ratio, '\t%'+bc.ENDC)
+        else:
+            print("%08.8f" % i[1], '\t', bc.OKGREEN, "%08.8f" % i[0], bc.ENDC)
+
+    print('\n')
 
     buy_edge_price = exchange_orderbook['buy'][0][0]
     buy_edge_amount = exchange_orderbook['buy'][0][1]
@@ -264,17 +371,14 @@ while True:
 
     # print(bc.ENDC)
 
-
     # print(bc.WARNING, sell_edge_amount, '\t', sell_edge_price, bc.ENDC)
-    
+
     # print(bc.DARKGREY+'Exchange Rate:\t', last_price, bc.ENDC)
 
     # print(bc.OKGREEN, buy_edge_amount, '\t', buy_edge_price, bc.ENDC)
-    
-
 
     sell_or_buy = input(
-        'Buy(1) | Sell(2) | Shift BUY Robots(3) | Shift SELL Robots(4) | My Open Orders(5) | Select Market(6) | Quit(q)? ')
+        'Buy(1) | Sell(2) | Shift BUY Robots(3) | Shift SELL Robots(4) | My Open Orders(5) | Select Market(6) | Select Manual Order Size(7) | Quit(q)? ')
 
     clear()
 
@@ -289,10 +393,10 @@ while True:
         # Calculate minimum trade size
         min_trade_size = round(MIN_BTC_TRADE/limit_price, 8)
         print('\nLimit Price:\n'+bc.BOLD)
-        print(limit_price, bc.ENDC)
+        print("%08.8f" % limit_price, bc.ENDC)
 
         print(bc.OKGREEN+'\nAmount to BUY:\n'+bc.BOLD)
-        print(min_trade_size)
+        print("%08.8f" % min_trade_size)
         print(bc.ENDC)
 
     elif sell_or_buy == '2':  # Display SELL advice
@@ -303,35 +407,48 @@ while True:
         min_trade_size = round(MIN_BTC_TRADE/limit_price, 8)
 
         print('\nLimit Price:\n'+bc.BOLD)
-        print(limit_price, bc.ENDC)
+        print("%08.8f" % limit_price, bc.ENDC)
 
         print(bc.WARNING+'\nAmount to SELL:\n'+bc.BOLD)
-        print(min_trade_size)
+        print("%08.8f" % min_trade_size)
         print(bc.ENDC)
 
     elif sell_or_buy == '3':  # Making Robot Shifting Orders SELL
         # Do orders
         # Cancel previous exchange order
-        if 'my_exchange_orders' in globals(): # Check that previous order was created programmatically
-            cancelled_order = private_api.cancel_exchange_order(exchange_info['symbols'][int(current_market)]['symbol'], my_exchange_orders[0]['orderId'])
-            print ('Previous Order Cancelled:')
-            print(cancelled_order)
-            print('\n')
+
+        # refresh latest trade date and advice calculations
+        limit_price, min_trade_size = get_latest_shift_advice('buy')
+        if manual_order_size:
+            min_trade_size = manual_order_size
+        # print (limit_price)
+        # print (min_trade_size)
+
+        if 'my_exchange_orders' in globals():  # Check that previous order was created programmatically
+            cancelled_order = private_api.cancel_exchange_order(exchange_info['symbols'][int(
+                current_market)]['symbol'], my_exchange_orders[0]['orderId'])
+            print('Previous Order Cancelled:'+bc.Magenta)
+            print("%08.8f" % cancelled_order['origQty'],
+                  '\t', "%08.8f" % cancelled_order['price'])
+            print(bc.ENDC)
 
         # # Create minimal sell limit exchange shift order
-        new_buy_limit_order = private_api.create_exchange_limit_order(exchange_info['symbols'][int(current_market)]['symbol'], 'buy', min_trade_size, limit_price)
-        print ('Creating new Shift BUY Order:'+bc.Magenta)
-        print (new_buy_limit_order['price'], new_buy_limit_order['origQty'])
-        print ('\n'+bc.ENDC)
-        
-        # Get my exchange orders
-        my_exchange_orders = private_api.get_my_exchange_orders(exchange_info['symbols'][int(current_market)]['symbol'],'open')
+        new_buy_limit_order = private_api.create_exchange_limit_order(
+            exchange_info['symbols'][int(current_market)]['symbol'], 'buy', min_trade_size, limit_price)
+        print('Creating new Shift BUY Order:'+bc.Magenta)
+        print("%08.8f" % new_buy_limit_order['origQty'],
+              '\t', "%08.8f" % new_buy_limit_order['price'])
+        print(bc.ENDC)
+
+        # Get my exchange orders to cancel latest one in the begining
+        my_exchange_orders = private_api.get_my_exchange_orders(
+            exchange_info['symbols'][int(current_market)]['symbol'], 'open')
         # print (my_exchange_orders)
         # print ('\n')
 
-        wait = input('Hit Enter to continue:') # pause execution till user hit Enter key
+        # wait = input('Hit Enter to continue:') # pause execution till user hit Enter key
 
-    elif sell_or_buy == '4': # Making Robot Shifting Orders BUY
+    elif sell_or_buy == '4':  # Making Robot Shifting Orders BUY
         pass
         # # Create buy limit exchange order
         # new_sell_limit_order = private_api.create_exchange_limit_order(exchange_info['symbols'][0]['symbol'], 'sell', 10, 0.1)
@@ -339,9 +456,11 @@ while True:
         # print('\n')
 
     elif sell_or_buy == '5':  # List all my Open Orders
-        list_all_my_open_orders()
+        list_my_all_open_orders()
     elif sell_or_buy == '6':  # Select coin
         current_market = market_select()
+    elif sell_or_buy == '7':  # Select Order size manually
+        manual_order_size = input('Enter order size:')
     else:
         exit()
 
